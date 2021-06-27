30802c7e31505737359aaf5e502d86912fdb64b3
Maciej <58191262+MaciejWolak@users.noreply.github.com>
2021-06-27 12:23:02 +0200

    Update ProductController.java
    add heroku url

--- src/main/java/com/example/webshopapp/controller/ProductController.java
+++ src/main/java/com/example/webshopapp/controller/ProductController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("https://anggtest.herokuapp.com")
 @RestController
 @RequestMapping("/")
 public class ProductController {
